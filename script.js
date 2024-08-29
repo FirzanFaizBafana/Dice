a3edb824518613f781af465be156bf929ad4ee10
Firzan Faiz Bafana <faizfirzan7@gamil.com>
2024-08-29 18:01:28 +0700

    Dice

---
+++ script.js
@@ -0,0 +1,23 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+document
+  .querySelector(".img1")
+  .setAttribute("src", "images/" + randomDiceImage);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+
+document
+  .querySelector(".img2")
+  .setAttribute("src", "images/" + randomDiceImage2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+} else {
+  document.querySelector("h1").innerHTML = "It's a Draw!";
+}
